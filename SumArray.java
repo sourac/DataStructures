b50d686c9c5044476087c9ab0cdcddcc4442a4db
Atul Sharma <atulsharma21772@gmail.com>
2016-06-17 11:39:02 +0530

    Added Count pair with sum array
    This program compute the number of pairs in the array with the given sum and thus return you the number of counts.

---
+++ SumArray.java
@@ -0,0 +1,28 @@
+/*this program has the functionality of findind the number of times 
+ * the given sum can be formed in the array through pairing two elements.
+ * */
+
+package Arrays;
+public class SumArray {
+public static void main(String[] args) {
+	
+	int input[]={1, 5, 7, -1, 5};
+	int sum=6;
+	pairOfSum(input,sum);
+	
+}
+
+private static void pairOfSum(int[] input, int sum) {
+	int count=0;
+	for(int i=0;i<input.length;i++)
+	{
+		for(int j=i+1;j<input.length;j++){
+			if((input[i]+input[j])==sum)
+			count++;
+		}
+	}
+	System.out.println("count is : "  + count);
+	
+}
+}
+
